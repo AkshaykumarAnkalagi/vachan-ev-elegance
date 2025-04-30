e8b2077e90496ed472b716e4573cd28da6107955
siva0704 <vaibhavabg1234@gmail.com>
2025-04-30 11:03:46 +0530

    'l'

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter  basename="/vachana/">
+      <BrowserRouter  basename="/vachana">
         <ScrollToTop />
         <Navbar />
         <main>
