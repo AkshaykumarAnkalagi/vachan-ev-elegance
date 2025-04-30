c79ad12ab786f0d32024df7a57348fa2685371ab
siva0704 <vaibhavabg1234@gmail.com>
2025-04-30 10:52:20 +0530

    l

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/vachana/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
